9e3ccd196f7599f75f23359b642a138b069c8942
7rans <transfire@gmail.com>
2011-10-16 11:02:15 -0400

    Add require for detroit/tool.

--- lib/detroit-minitest.rb
+++ lib/detroit-minitest.rb
@@ -1,4 +1,11 @@
+require 'detroit/tool'
+
 module Detroit
+
+  # Convenience method for creating a new MiniTest instance.
+  def MiniTest(options={})
+    MiniTest.new(options)
+  end
 
   # The MiniTest tool runs tests. For Ruby 1.9+ `minitest/autorun` is
   # used to run the tests. For older versions of Ruby, `test/unit` is used.
@@ -8,34 +15,96 @@ module Detroit
     # By default look for tests in test directory.
     DEFAULT_TESTS = ['test/*_test.rb', 'test/test_*.rb']
 
+    #  A T T R I B U T E S
+
     # Test files to include.
-    attr_accessor :tests
+    attr_reader :tests
+
+    #
+    def tests=(paths)
+      @tests = [paths].flatten
+    end
 
     # Test files or globs to ignore.
-    attr_accessor :exclude
+    attr_reader :exclude
+
+    #
+    def exclude=(paths)
+      @exclude = [paths].flatten
+    end
 
     # Test files to ignore by matching filename.
-    attr_accessor :ignore
+    attr_reader :ignore
+
+    #
+    def ignore=(paths)
+      @ignore = [paths].flatten
+    end
+
+    #
+    attr_reader :loadpath
+
+    #
+    def loadpath=(paths)
+      @loadpath = [paths].flatten
+    end
+
+    #
+    attr_reader :requires
+
+    #
+    def requires=(paths)
+      @requires = [paths].flatten
+    end
+
+    #
+    attr_accessor :extra
+
+    #  A S S E M B L Y  S T A T I O N S
+
+    #
+    def station_test
+      test
+    end
+
+    #  S E R V I C E  M E T H O D S
 
     # Run tests.
     def test
-      requires = [autorunner] + resolved_tests
+      requires = [autorunner] + self.requires
 
-      cmd = "-e '" + requires.map{ |f| %(require "#{f}; ") } + "'"
+      cmd = []
+      loadpath.each do |path|
+        cmd << "-I#{path}"
+      end     
+      requires.each do |path|
+        cmd << "-r#{path}"
+      end
+      cmd << "-e '%w{" + resolved_tests.join(' ') + "}.each{ |f| require f }'"
+      cmd << extra.to_s if extra
 
-      ruby cmd
+      success = ruby(cmd.join(' '))
+
+      abort "Aborted due to test failures. Use --force to override." unless success or force?
     end
 
-    private
+  private
 
     #
     def initialize_defaults
-      @tests = DEFAULT_TESTS
+      @tests    = DEFAULT_TESTS
+      @exclude  = []
+      @ignore   = []
+      @loadpath = []
+      @requires = []
     end
 
     # Resolve test globs.
     def resolved_tests
-      @resolved_tests ||= amass(tests, exclude, ignore)
+      @resolved_tests ||= (
+        list = amass(tests, exclude, ignore)
+        list.map{ |f| "./#{f}" }
+      )
     end
 
     #
